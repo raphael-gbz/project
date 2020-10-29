cdc63df4f764e1c9ee2ed1e62598606532461e6c
Raphael Gbz <qsdwz@yahoo.fr>
2020-10-29 10:53:45 +0100

    make if better

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '0e9ba98bf465a8d69b3f7a2d04f5daa57b37e752',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0e9ba98bf465a8d69b3f7a2d04f5daa57b37e752',
+    ),
+  ),
+);
